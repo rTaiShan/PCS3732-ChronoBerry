da181b2d908735001c0d875d5fbf3f8ec9fe99e9
mateuswiteck <mateuswiteck@gmail.com>
2024-07-24 15:02:35 -0300

    pinMode e DigitalWrite

--- src/GPIO.c
+++ src/GPIO.c
@@ -2,88 +2,43 @@
 #include <stdint.h>
 #include "GPIO.h"
 
-volatile uint32_t* gpio_set = (volatile uint32_t*) GPIO_SET_REG; // Pointer to the GPSET0
-volatile uint32_t* gpio_clr = (volatile uint32_t*) GPIO_CLR_REG; // Pointer to the GPCLR0
+#define HIGH 0x1
+#define LOW  0x0
 
-void config_gpio_output(int pin) {
-    asm volatile(
-        "push {r1, r2, r3}            \n"
-        "ldr r0, =%[gpfsel0]          \n"
-        "add r0, r0, %1               \n"
-        "ldr r1, [r0]                 \n"
-        "mov r2, %2                   \n"
-        "mov r3, #7                   \n"
-        "lsl r3, r3, r2               \n"
-        "bic r1, r1, r3               \n"
-        "mov r3, #1                   \n"
-        "lsl r3, r3, r2               \n"
-        "orr r1, r1, r3               \n"
-        "str r1, [r0]                 \n"
-        "pop {r1, r2, r3}             \n"
-        :
-        : [gpfsel0] "i" (GPFSEL0), "r" ((pin / 10) * 4), "r" ((pin % 10) * 3)
-        : "r0", "r1", "r2", "r3", "memory"
-    );
+#define INPUT 0x0
+#define OUTPUT 0x1
+
+void pinMode(uint8_t pin, uint8_t mode) {
+    /*
+    * Configura o pin da GPIO como saída (001) ou entrada (000)
+    */
+
+    uint8_t reg_num = pin/10;
+    uint8_t offset = 3 * (pin%10);
+
+    // Estado atual da configuração:
+    uint32_t fsel = GPIO_REG(gpfsel[reg_num]);
+
+    if (mode == OUTPUT) {
+        GPIO_REG(gpfsel[reg_num]) = (fsel & (~(0x7 << offset))) | (1 << (0x7 << offset));
+    } else if (mode == INPUT) {
+        GPIO_REG(gpfsel[reg_num]) = (fsel & (~(0x7 << offset))) | (0 << (0x7 << offset));
+    }
 }
 
-void config_gpio_input(int pin) {
-    asm volatile(
-        "push {r1, r2, r3}            \n"
-        "ldr r0, =%[gpfsel0]          \n"
-        "add r0, r0, %1               \n"
-        "ldr r1, [r0]                 \n"
-        "mov r2, %2                   \n"
-        "mov r3, #7                   \n"
-        "lsl r3, r3, r2               \n"
-        "bic r1, r1, r3               \n"
-        "str r1, [r0]                 \n"
-        "pop {r1, r2, r3}             \n"
-        :
-        : [gpfsel0] "i" (GPFSEL0), "r" ((pin / 10) * 4), "r" ((pin % 10) * 3)
-        : "r0", "r1", "r2", "r3", "memory"
-    );
+// TODO Proper digitalWrite
+void digitalWrite(uint8_t pin, uint8_t val) {
+    // liga a saída 18 e desliga a saída 19
+    if (val == HIGH){
+        GPIO_REG(gpset[0]) = 0x01 << pin;
+    } else if (val == LOW){
+        GPIO_REG(gpclr[0]) = 0x01 << pin;
+    }
 }
 
-uint32_t read_gpio_pin(int pin) {
-    uint32_t value;
-    uint32_t mask = 1 << pin;
+// TODO Proper digitalRead
+uint8_t digitalRead(uint8_t pin) {
 
-    asm volatile (
-        "ldr %[value], [%[gplev0]]\n"     // Carrega o valor do registrador GPLEV0
-        "and %[value], %[value], %[mask]\n" // Mascara o bit específico do pino
-        "cmp %[value], #0\n"              // Compara o valor mascarado com 0
-        "moveq %[value], #0\n"            // Se o valor for 0, value é 0
-        "movne %[value], #1\n"            // Se o valor não for 0, value é 1
-        : [value] "=r" (value)
-        : [gplev0] "r" (GPLEV0), [mask] "r" (mask)
-        : "memory"
-    );
 
-    return value;
+    return 1;
 }
-
-void setGPIOpin(uint8_t pin) {
-    asm volatile(
-        "mov r0, %[pin]\n"            // Move pin number into r0
-        "ldr r1, %[set]\n"            // Load the address of GPIO_SET_REG into r1
-        "mov r2, #1\n"                // Move 1 into r2
-        "lsl r2, r0\n"                // Left shift r2 by r0 (pin number)
-        "str r2, [r1]\n"              // Store r2 into the address contained in r1 (GPIO_SET_REG)
-        : 
-        : [pin] "r" (pin), [set] "m" (gpio_set) // Input operands
-        : "r0", "r1", "r2"            // Clobbered registers
-    );
-}
-
-void clearGPIOpin(uint8_t pin) {
-    asm volatile(
-        "mov r0, %[pin]\n"            // Move pin number into r0
-        "ldr r1, %[clr]\n"            // Load the address of GPIO_CLR_REG into r1
-        "mov r2, #1\n"                // Move 1 into r2
-        "lsl r2, r0\n"                // Left shift r2 by r0 (pin number)
-        "str r2, [r1]\n"              // Store r2 into the address contained in r1 (GPIO_CLR_REG)
-        : 
-        : [pin] "r" (pin), [clr] "m" (gpio_clr) // Input operands
-        : "r0", "r1", "r2"            // Clobbered registers
-    );
-}       
--- src/GPIO.h
+++ src/GPIO.h
@@ -1,23 +1,17 @@
-#ifndef __GPIO_H__
-#define __GPIOH__
-#include "bcm.h"
 
-#define GPFSEL0 ((volatile uint32_t *)(GPIO_ADDR + 0x00))
 
-void config_gpio_output(int pin);
-void config_gpio_input(int pin);
+#include <stdint.h>
 
-#define GPLEV0 ((volatile uint32_t *)(GPIO_ADDR + 0x34)) // Endereço de Leitura das portas
+#define HIGH 0x1
+#define LOW  0x0
 
-uint32_t read_gpio_pin(int pin);
+#define INPUT 0x0
+#define OUTPUT 0x1
 
-#define GPIO_SET_REG (GPIO_ADDR + 0x1C) // GPSET0 register address
-#define GPIO_CLR_REG (GPIO_ADDR + 0x28) // GPCLR0 register address
+void pinMode(uint8_t pin, uint8_t mode);
 
-#define GPCLR1 (GPIO_ADDR + 0x2C)
 
-void setGPIOpin(uint8_t pin);
-void clearGPIOpin(uint8_t pin);
+void digitalWrite(uint8_t pin, uint8_t val);
 
-#define GPCLR0 (GPIO_ADDR + 0x28)
-#endif
+
+uint8_t digitalRead(uint8_t pin);
